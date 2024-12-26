569451bbd2912d4d758294631479f981054cb804
Connor Sullivan <dev.connorjs@gmail.com>
2024-12-26 11:11:13 -0500

    ♻️ SSG + refactoring (#19)
    Enables prerender (SSG, static site generation). Refactors the loader
    page to be a generic component page. Replaces dynamic import with direct
    import. Fixes 404 catchall page.

---
+++ website/src/components/component-page.tsx
@@ -0,0 +1,45 @@
+/* c8 ignore start -- I will add unit tests once the code settles. */
+import {
+	Card,
+	CardBody,
+	CardHeader,
+	CardTitle,
+	StackLayout,
+} from "@progress/kendo-react-layout";
+import type { JSX } from "react";
+
+export type ComponentsPageProps = {
+	componentName: string;
+	documentation: unknown;
+	example: JSX.Element;
+};
+
+export function ComponentPage(props: Readonly<ComponentsPageProps>) {
+	return (
+		<section>
+			<div>
+				<h2>{props.componentName}</h2>
+			</div>
+			<StackLayout orientation="vertical" gap="var(--kendo-spacing-5)">
+				<Card>
+					<CardHeader>
+						<CardTitle>Example</CardTitle>
+					</CardHeader>
+					<CardBody>{props.example}</CardBody>
+				</Card>
+				<Card>
+					<CardHeader>
+						<CardTitle>Documentation</CardTitle>
+					</CardHeader>
+					<CardBody>
+						<pre>
+							<code>{JSON.stringify(props.documentation, null, 2)}</code>
+						</pre>
+						)
+					</CardBody>
+				</Card>
+			</StackLayout>
+		</section>
+	);
+}
+/* c8 ignore stop -- END */
