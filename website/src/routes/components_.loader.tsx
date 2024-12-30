e09f313c8742c1f92354231d248b59719ef2d24a
Connor Sullivan <dev.connorjs@gmail.com>
2024-12-30 14:22:31 -0500

    🐛 Fix loader page
    Fixes loader page.

--- website/src/routes/components_.loader.tsx
+++ website/src/routes/components_.loader.tsx
@@ -1,8 +1,9 @@
+import { Loader } from "@progress/kendo-react-indicators";
 import { ComponentPage } from "~/components/component-page.tsx";
 // @ts-expect-error -- No typing for JS dynamic import.
 import indicators from "~/kendo-docs/indicators.js";
 
-export default function Loader() {
+export default function LoaderPage() {
 	return (
 		<ComponentPage
 			componentName="Loader"
